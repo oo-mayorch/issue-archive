3f6efdfadec1db4053fe71ee0682e5ba3273ad2d
mayorch <mayorch@oocl.com>
2026-02-26 15:06:13 +0800

    Setup backend for fullstack project

---
+++ src/main/java/com/issueresolution/issue_archive/IssueArchiveApplication.java
@@ -0,0 +1,14 @@
+package com.issueresolution.issue_archive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IssueArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issueresolution/issue_archive/IssueArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issueresolution.issue_archive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
