454b75ef2b02148c1a6f6af9609a27f7257f92d7
mayorch <mayorch@oocl.com>
2026-03-02 09:02:28 +0800

    Created IssueRepository and IssueService. Will need to be put in hold to create module first

---
+++ src/main/java/com/issueresolution/issue_archive/service/impl/IssueServiceImpl.java
@@ -0,0 +1,7 @@
+package com.issueresolution.issue_archive.service.impl;
+
+import com.issueresolution.issue_archive.dto.*;
+import com.issueresolution.issue_archive.entity.*;
+
+public class IssueServiceImpl {
+}
